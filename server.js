a41aa09f8ae9ba8c8c3b4a71424e32151959105e
Megan Coyle <mcoyle@usgbc.org>
2017-04-14 08:31:11 -0400

    Add api set up

---
+++ server.js
@@ -0,0 +1,18 @@
+import config from './config';
+import apiRouter from './api';
+
+import express from 'express';
+const server = express();
+
+server.set('view engine', 'ejs');
+
+server.get('/', (req, res) => {
+  res.send('Hello Express');
+});
+
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+server.listen(config.port, ()=> {
+  console.info('Express is listening on port ', config.port);
+});
