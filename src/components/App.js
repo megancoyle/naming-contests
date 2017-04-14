6ed9d2e356d76a13f08ae4006fd1dae695fd51be
Megan Coyle <mcoyle@usgbc.org>
2017-04-14 10:03:50 -0400

    Connect to test data

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import Header from './Header';
+import ContestPreview from './ContestPreview';
+import data from '../testData';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header message="Naming Contests"/>
-      <div>
-        ...
+class App extends React.Component {
+  state = {
+    pageHeader: 'Naming Contests',
+    contests: []
+  };
+  componentDidMount() {
+    this.setState({
+      contests: data.contests
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header message={this.state.pageHeader}/>
+        <div>
+        {this.state.contests.map(contest =>
+          <ContestPreview key={contest.id} {...contest} />
+        )}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
