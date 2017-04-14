5398eefb859be7e5b3e204393bf661f8117a4863
Megan Coyle <mcoyle@usgbc.org>
2017-04-14 08:44:58 -0400

    Set up views

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const server = express();
 server.set('view engine', 'ejs');
 
 server.get('/', (req, res) => {
-  res.send('Hello Express');
+  res.render('index', {
+    content: "Hello Express"
+  });
 });
 
 server.use('/api', apiRouter);
