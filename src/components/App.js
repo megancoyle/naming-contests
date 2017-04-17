22c7bf6b7aa647f834dfcb34fea8607d4be46341
Megan Coyle <mcoyle@usgbc.org>
2017-04-17 15:04:07 -0400

    Set up ReactDOMserver

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import axios from 'axios';
 import Header from './Header';
 import ContestPreview from './ContestPreview';
 
 class App extends React.Component {
   state = {
     pageHeader: 'Naming Contests',
-    contests: []
+    contests: this.props.initialContests
   };
   componentDidMount() {
-    axios.get('/api/contests')
-      .then(resp => {
-        this.setState({
-          contests: resp.data.contests
-        });
-      })
-      .catch(console.error)
+
+  }
+
+  componentWillUnmount() {
 
   }
 
